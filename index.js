993d625b88b6b6e3cb01fa085801dd58943860da
Tekyto <Tekyto2014@gmail.com>
2022-11-20 10:13:42 +0200

    js_27_sum-of-object++

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 function sumOfObject(store) {
-	// write your code here
+	let sum = 0;
+
+	for (let i = 0; i < Object.values(store).length; i++) {
+		sum += Object.values(store)[i];
+	}
+	return sum;
 }
 
 module.exports = sumOfObject;
